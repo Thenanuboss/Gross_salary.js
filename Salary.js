ae8fe2c9039b1c4bc8b3354fad5a3af2ab262ff4
Thenanuboss <163496069+Thenanuboss@users.noreply.github.com>
2024-11-27 10:17:09 +0530

    Add files via upload

---
+++ Salary.js
@@ -0,0 +1,10 @@
+// Gross salary
+
+let salary = 15000;
+let hr = 20;
+let da = 5;
+let ta = 8;
+
+let totalsalary = salary + (salary*hr)/100 + (salary*da)/100 + (salary*ta)/100;
+
+console.log(totalsalary)
